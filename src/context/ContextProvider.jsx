09b93a901cf4129cebeb2bcf0dc99fa3af1e062d
dawit2123 <dawitzewdu2123@gmail.com>
2024-07-16 09:30:19 +0300

    chore: Update import statement for Africhat module

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -1,5 +1,5 @@
 "use client";
-import runChat from "@/lib/gemini";
+import runChat from "@/lib/africhat";
 import React, { createContext, useState } from "react";
 
 export const Context = createContext();
