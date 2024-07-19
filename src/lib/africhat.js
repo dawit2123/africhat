313b357817c5ad3f07984e794b1b7ea37fc1fe13
dawit2123 <dawitzewdu2123@gmail.com>
2024-07-19 16:57:13 +0300

    first version of africhat done

--- src/lib/africhat.js
+++ src/lib/africhat.js
@@ -8,7 +8,7 @@ import {
 } from "@google/generative-ai";
 
 const MODEL_NAME = "gemini-1.0-pro";
-const API_KEY = "AIzaSyAbSRcz1tdSQEU_XEyUa5PjnT9BimnOe2w";
+const API_KEY = "YOUR_API_KEY";
 
 async function runChat(prompt) {
   const genAI = new GoogleGenerativeAI(API_KEY);
